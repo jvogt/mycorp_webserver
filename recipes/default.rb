bb7823e8686500df5a78c602c6ea9bec1921f7d2
Your Name <you@example.com>
2019-10-03 10:25:53 -0700

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+    action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+    content 'Hello World'
+end

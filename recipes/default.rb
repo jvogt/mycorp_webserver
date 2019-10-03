a1150a7a823bd401d4a1755e13511bc01d84d00e
Your Name <you@example.com>
2019-10-03 13:47:40 -0700

    add chef user

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,26 @@ end
 file '/var/www/html/index.html' do
   content "Hello World from #{node.policy_group}"
 end
+
+directory '/home/chef'
+
+user 'chef' do
+  home '/home/chef'
+  manage_home true
+  password '$1$WgXqffFX$KeByLO7vY1tUuOFwim7oq0'
+end
+
+sudo 'chef' do
+  user 'chef'
+  nopasswd true
+end
+
+service 'sshd' do
+  action :nothing
+end
+
+execute "sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config" do
+  only_if 'grep "^PasswordAuthentication no" /etc/ssh/sshd_config'
+  action :run
+  notifies :restart, 'service[sshd]', :immediately
+end
